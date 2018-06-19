28464db8379cd1c87a037afc1d2d00d0b7581203
Jonathan Mai <h.jmai2791@gmail.com>
2018-06-18 21:02:07 -0700

    Finished train scheduler

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ $('#add-employee-btn').on('click', function () {
     event.preventDefault();
     var trainName = $('#trainName').val().trim();
     var destination = $('#destination').val().trim();
-    var firstTime = $('#firstTime').val().trim();
+    var firstTime = moment($("#firstTime").val().trim(), "HH:mm").subtract(10, "years").format("X");
+    console.log(firstTime);
     var frequency = $('#frequency').val().trim();
     
     $('#trainName').val('');
@@ -32,26 +33,22 @@ $('#add-employee-btn').on('click', function () {
 });
 
 database.ref().on('child_added', function(childSnapshot){
-    console.log(childSnapshot);
     var row = $('<tr>');
     var trainNameCell = $('<td>').text(childSnapshot.val().trainName);
     var destinationCell = $('<td>').text(childSnapshot.val().destination);
-    var firstTimeCell = $('<td>').text(childSnapshot.val().firstTime);
     var frequencyCell = $('<td>').text(childSnapshot.val().frequency);
+    
+    // Calculate Times
+    var timeRemainder = moment().diff(moment.unix(childSnapshot.val().firstTime), "minutes") % childSnapshot.val().frequency;
+    var minutes = childSnapshot.val().frequency - timeRemainder;
+    var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A");
+    var minutesAwayCell = $('<td>').text(minutes);
+    var nextArrivalCell = $('<td>').text(nextTrainArrival);
+    
     row.append(trainNameCell);
     row.append(destinationCell);
     row.append(frequencyCell);
-    
+    row.append(nextArrivalCell);
+    row.append(minutesAwayCell);
     $('.tableBody').append(row);
 });
-
-function nextArrival(firstArrival){
-    var timeFormat = "HH:mm";
-    
-}
-
-function convertTime(date){
-    var dateFormat = "DD/MM/YY";
-    var convertedDate = moment(date, dateFormat);
-    return Math.abs(moment(convertedDate).diff(moment(), 'months'));
-}
